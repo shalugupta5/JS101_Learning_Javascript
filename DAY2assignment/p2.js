931fec7efc3eb6646eb181e00839f962ff914b5a
shalu <shalug123456@gmail.com>
2022-09-04 12:15:21 +0530

    Pushing Mathematical_Operator

---
+++ DAY2assignment/p2.js
@@ -0,0 +1,7 @@
+var one,two,three,four,five;
+one=1;
+two=2;
+three=3;
+four=4;
+five=5;
+console.log((one*2)+(two*2)+(three*2)+(four*2)+(five*2));
---
+++ DAY2assignment/p3.js
@@ -0,0 +1,8 @@
+var one,two,three,four,five,six;
+one=1;
+two=2;
+three=3;
+four=4;
+five=5;
+six=6;
+console.log(one+(two*2)+(three*3)+(four*3)+(five*2)+(six*2));
